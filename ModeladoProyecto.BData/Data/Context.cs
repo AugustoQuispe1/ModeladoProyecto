a0ab43383d7f3e5722a185c1e1a167e0313b0d14
AugustoQuispe1 <augustoq93@gmail.com>
2023-08-26 20:16:37 -0300

    Add project files.

---
+++ ModeladoProyecto.BData/Data/Context.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModeladoProyecto.BData.Data.Entidades;
+
+namespace ModeladoProyecto.BData.Data
+    {
+    public class Context : DbContext
+        {
+
+        public DbSet<Stock> stock => Set<Stock>();
+
+        public Context(DbContextOptions options) : base(options)
+            {
+            }
+
+        }
+    }
---
+++ ModeladoProyecto.BData/Data/Entidades/Stock.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+
+namespace ModeladoProyecto.BData.Data.Entidades
+    {
+    [Index(nameof(CodStock), Name = "CodStock_UQ", IsUnique = true)]
+    public class Stock
+        {
+
+        [Required(ErrorMessage = "El Código del producto es obligatorio")]
+        [MaxLength(3, ErrorMessage = "Solo se aceptan hasta 3 caracteres en el codigo del producto")]
+        public string CodStock { get; set;}
+   
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Es necesario introducir el producto obligatorio")]
+        [MaxLength(30, ErrorMessage = "Solo se aceptan hasta 30 caracteres en el producto")]
+        public string Producto { get; set; }
+
+        [Required(ErrorMessage = "Es necesario introducir el producto obligatorio")]
+        [MaxLength(4, ErrorMessage = "Solo se aceptan hasta 30 caracteres en la cantidad")]
+        public int Cantidad { get; set; }
+
+        }
+    }
